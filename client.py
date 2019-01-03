14047f9f41189089d4c676cc37c95181895e195a
momo <Moritz Krger>
2019-01-03 17:48:43 +0100

    added while true loop

--- client.py
+++ client.py
@@ -7,13 +7,13 @@ context = zmq.Context()
 print("Connecting to hello world server")
 socket = context.socket(zmq.REQ)
 socket.connect("tcp://localhost:5555")
-
+request=0
 #  Do 10 requests, waiting each time for a response
-for request in range(10):
+while(True):
+    request+=1
     print("Sending request %s" % request)
     socket.send(b"Hello")
 
     #  Get the reply.
     message = socket.recv()
     print("Received reply %s [ %s ]" % (request, message))
-return 0
