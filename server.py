c15e27533500682c087febcc67f3f190633b5028
momo <Moritz Krger>
2019-01-03 17:04:59 +0100

    changed print behaviour for kubernetes

--- server.py
+++ server.py
@@ -10,7 +10,7 @@ import zmq
 context = zmq.Context()
 socket = context.socket(zmq.REP)
 socket.bind("tcp://*:5555")
-
+print("hello my friend")
 while True:
     #  Wait for next request from client
     message = socket.recv()
